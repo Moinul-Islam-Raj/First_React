6bae9fa25b5c3292e749a92f6c1f4cb32402a6d9
Moinul Islam Raj <moinulislamraj0@gmail.com>
2023-07-30 20:22:45 +0600

    counter rm

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import Counter from "./components/Counter";
-
 function App() {
-  return (
-    <div>
-      <Counter keys="c1" imgSrc="/vite.svg" />
-      <Counter keys="c2" imgSrc="/react.svg" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
