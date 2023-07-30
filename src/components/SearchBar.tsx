0b4ba677eb9991ae8073a8c25ed329ba7427f969
Moinul Islam Raj <moinulislamraj0@gmail.com>
2023-07-30 20:53:14 +0600

    final product css

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,7 +3,11 @@ import "./css/searchBar.css";
 const SearchBar = () => {
   return (
     <>
-      <form id="search-google" action="https://www.google.com/search">
+      <form
+        id="search-google"
+        action="https://www.google.com/search"
+        target="_blank"
+      >
         <div>
           <label htmlFor="g-search-bar">Google Search</label>
           <input type="search" name="q" id="g-search-bar" />
